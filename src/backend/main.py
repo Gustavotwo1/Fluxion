1c5280a018f93b1d08d60b2ce41219769ffbadb8
Eddye <141441523+Eddye-Robert@users.noreply.github.com>
2026-06-21 18:37:36 -0300

    Update main.py
    criando o deploy

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,1 +1,17 @@
+import os
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
+class MeuServidor(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        mensagem = "<h1>Deploy realizado com sucesso! Aguardando o código da equipe.</h1>"
+        self.wfile.write(mensagem.encode("utf-8"))
+
+if __name__ == "__main__":
+    # O Render define automaticamente qual porta o sistema deve usar
+    porta = int(os.environ.get("PORT", 8000))
+    servidor = HTTPServer(("0.0.0.0", porta), MeuServidor)
+    print(f"Servidor online na porta {porta}...")
+    servidor.serve_forever()
